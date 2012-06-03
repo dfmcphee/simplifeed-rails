cc798abeacfeb9dd24fcef6524d7e430b34b334b
Dominic McPhee <dominic.mcphee@gmail.com>
2012-06-03 14:01:16 -0400

    Fixes to post layout in feed. Also added timeago.js to format timestamps into a twitter like format.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Simplifeed::Application.routes.draw do
-  devise_for :users
+  devise_for :users do
+  	get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
 
   root :to => 'users#show'
 
