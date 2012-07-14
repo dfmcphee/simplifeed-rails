1ff5c7fe9b78439552ef1d3626eadba61f2c438a
Dominic McPhee <dominic.mcphee@gmail.com>
2012-07-14 14:30:02 -0400

    Addressing time zone issue. Forcing easter time for now. Will allow users to pick in future.

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,11 +34,7 @@ namespace :deploy do
   namespace :assets do
         task :precompile, :roles => :web, :except => { :no_release => true } do
           from = source.next_revision(current_revision)
-          if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-            run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-          else
-            logger.info "Skipping asset pre-compilation because there were no asset changes"
-          end
+          run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} assets:precompile}
         end
    end
 end
